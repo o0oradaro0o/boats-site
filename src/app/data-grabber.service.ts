78c951150586f512a5118ff8a314b34ff770ad8b
David Page \6186 <david.page@omnicell.com>
2019-03-13 13:07:46 -0700

    made httpOptions a property. Please enter the commit message for your changes. Lines starting

--- src/app/data-grabber.service.ts
+++ src/app/data-grabber.service.ts
@@ -1,11 +1,10 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
-import { HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { GameDetailContent } from './models/game-detail';
-
 import { GameContent } from './models/game-simple';
+
 
 
 const apiUrl = 'https://grdxgi2qm1.execute-api.us-east-1.amazonaws.com/battleships/';
@@ -16,28 +15,22 @@ const apikey = 'FX5Tqd1joL2CC3p1tjCoF7hJCIoRrNDv4m0tqmvo';
   providedIn: 'root'
 })
 export class DataGrabberService {
-  constructor(route: ActivatedRoute, private http: HttpClient) {}
-  getGames(): Observable<GameContent> {
-    // One of several ways to set up HTTP request URL parameters
-    // without concatenating them manually.
-      const httpOptions = {
-        headers: new HttpHeaders({
-          'x-api-key':  apikey,
-        })};
 
+  httpOptions = {
+    headers: new HttpHeaders({
+      'x-api-key':  apikey,
+    })};
+
+  constructor(route: ActivatedRoute, private http: HttpClient) {}
+
+  getGames(): Observable<GameContent> {
       return this.http.get<GameContent>(apiUrl +
         'battleships_game?include=numPlayers,wn,settings,matchID,dateProcessed,gameDuration&take=100',
-        httpOptions);
+        this.httpOptions);
   }
 
   getGameDetail(matchId: number): Observable<GameDetailContent> {
-    // One of several ways to set up HTTP request URL parameters
-    // without concatenating them manually.
-      const httpOptions = {
-        headers: new HttpHeaders({
-          'x-api-key':  apikey,
-        })};
       console.log('made a call for ' + matchId);
-      return this.http.get<GameDetailContent>(apiUrl + 'battleships/' + matchId, httpOptions);
+      return this.http.get<GameDetailContent>(apiUrl + 'battleships/' + matchId, this.httpOptions);
   }
 }
