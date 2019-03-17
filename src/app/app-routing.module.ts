0c0a5eb38ae20bc44f6a32b05ad493fadae15f3c
Josh Guthrie <josh.x.guthrie@gmail.com>
2019-03-16 21:23:14 -0500

    Introduced initial designs, added homepage component template and styles.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component'
 
 const routes: Routes = [
-  { path: '', redirectTo: 'dash', pathMatch: 'full' },
-  { path: 'dash', loadChildren: './stat-dashboard/stat-dashboard.module#StatDashboardModule' },
+  { path: '', component: HomepageComponent },
+  { path: 'stats', loadChildren: './stat-dashboard/stat-dashboard.module#StatDashboardModule' },
   { path: 'game', loadChildren: './game-dashboard/game-dashboard.module#GameDashboardModule' },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomepageComponent } from './homepage/homepage.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
