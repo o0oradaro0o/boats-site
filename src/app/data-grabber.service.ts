93290708f49a9a906d1dfdbc08a9ab9c693c98bc
lorne <l.vanatta@gmail.com>
2026-05-10 22:08:41 -0500

    refactor: replace hardcoded API URL and API key with environment variables in data-grabber.service.ts
    - Updated data-grabber.service.ts to use environment.battleshipsApiUrl for API calls.
    - Removed the API key from the service and the associated HttpHeaders.
    - Added battleshipsApiUrl to both environment.ts and environment.prod.ts for configuration.

--- src/app/data-grabber.service.ts
+++ src/app/data-grabber.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
@@ -7,21 +7,14 @@ import { GameDetailContent, GameDetail } from './models/game-detail';
 import { GameContent } from './models/game-simple';
 import { PlayerSimpleContent } from './models/player-simple';
 import { ItemRecordContent } from './models/player-item-record';
+import { environment } from '../environments/environment';
 
-const apiUrl =
-  'https://grdxgi2qm1.execute-api.us-east-1.amazonaws.com/battleships/';
-const apikey = 'FX5Tqd1joL2CC3p1tjCoF7hJCIoRrNDv4m0tqmvo';
+const apiUrl = environment.battleshipsApiUrl;
 
 @Injectable({
   providedIn: 'root',
 })
 export class DataGrabberService {
-  httpOptions = {
-    headers: new HttpHeaders({
-      'x-api-key': apikey,
-    }),
-  };
-
   constructor(
     route: ActivatedRoute,
     private http: HttpClient,
@@ -38,20 +31,18 @@ export class DataGrabberService {
       mm +
       dd +
       '?include=numPlayers,wn,settings,matchID,dateProcessed,gameDuration&take=200';
-    return this.http.get<GameContent>(s, this.httpOptions);
+    return this.http.get<GameContent>(s);
   }
 
   get300Games(): Observable<GameContent> {
     return this.http.get<GameContent>(
       apiUrl + 'query/8d442f4d-6bb1-11e9-b164-49ca76f2a334',
-      this.httpOptions,
     );
   }
 
   getGameDetail(matchId: number): Observable<GameDetailContent> {
     return this.http.get<GameDetailContent>(
       apiUrl + 'battleships/' + Math.abs(matchId),
-      this.httpOptions,
     );
   }
 
@@ -61,7 +52,6 @@ export class DataGrabberService {
         'battleships/' +
         Math.abs(matchId) +
         '?include=buildingDamage,connectionState,afk,playerName,lh,tm,loadTime,wn,shp,HeroDamage,buildOrder,dth,damageTanked,saleOrder,kls,lvl,boatOrder,playerID',
-      this.httpOptions,
     );
   }
   getGeneralGameDetail(matchId: number): Observable<GameDetailContent> {
@@ -70,45 +60,33 @@ export class DataGrabberService {
         'battleships/' +
         Math.abs(matchId) +
         '?include=numPlayers,gameDuration,empGoldHist,wn,combatLog,settings,matchID,dateProcessed&take=1',
-      this.httpOptions,
     );
   }
   getTopPlayers(): Observable<PlayerSimpleContent> {
     return this.http.get<PlayerSimpleContent>(
       apiUrl + 'query/582995a9-6a32-11e9-a89e-c70fc193172b',
-      this.httpOptions,
     );
   }
 
   getBoatData(): Observable<ItemRecordContent> {
     return this.http.get<ItemRecordContent>(
       apiUrl + 'query/e8a516bb-6eda-11e9-b8b5-e969e44c0733',
-      this.httpOptions,
     );
   }
   getItemData(): Observable<ItemRecordContent> {
     return this.http.get<ItemRecordContent>(
       apiUrl + 'query/56c4bfe4-6f42-11e9-b2b2-b91d96137316',
-      this.httpOptions,
     );
   }
 
   getPlayerItemData(playerId: number): Observable<ItemRecordContent> {
     const query = `{"query":"SELECT * FROM bships.playerBuildInfo where playerID='${playerId}'"}`;
-    return this.http.post<ItemRecordContent>(
-      `${apiUrl}query/`,
-      query,
-      this.httpOptions,
-    );
+    return this.http.post<ItemRecordContent>(`${apiUrl}query/`, query);
   }
 
   getPlayerBoatData(playerId: number): Observable<ItemRecordContent> {
     const query = `{"query":"SELECT * FROM bships.recentPlayerBoatInfo where playerID='${playerId}'"}`;
-    return this.http.post<ItemRecordContent>(
-      `${apiUrl}query/`,
-      query,
-      this.httpOptions,
-    );
+    return this.http.post<ItemRecordContent>(`${apiUrl}query/`, query);
   }
 
   getRecentMatches(playerId: number): Observable<GameDetail[]> {
@@ -116,7 +94,7 @@ export class DataGrabberService {
       'trading, battle, coOp, wn, tm, gameDuration, kls, dth, lvl, numPlayers, shp, dateProcessed, matchID';
     const query = `{"query":"SELECT top (20) ${columns} FROM bships.gamedata where playerID='${playerId}' and dedi=1 order by dateProcessed desc"}`;
     return this.http
-      .post<GameDetailContent>(`${apiUrl}query/`, query, this.httpOptions)
+      .post<GameDetailContent>(`${apiUrl}query/`, query)
       .pipe(map((res) => res.Content ?? []));
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  battleshipsApiUrl: 'https://vanatta.xyz/boats-api/',
 };
